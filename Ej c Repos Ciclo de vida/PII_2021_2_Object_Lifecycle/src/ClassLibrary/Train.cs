91b3168471ea7f22ae0df5bda16038faf1d4ec23
FelipeET <88661703+FelipeET@users.noreply.github.com>
2022-04-02 16:43:00 -0300

    Add files via upload

---
+++ Ej c Repos Ciclo de vida/PII_2021_2_Object_Lifecycle/src/ClassLibrary/Train.cs
@@ -0,0 +1,84 @@
+﻿//--------------------------------------------------------------------------------
+// <copyright file="Train.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
+using System;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Esta clase representa un tren muy básico.
+    /// </summary>
+    public class Train
+    {
+        /// <summary>
+        /// Obtiene un valor que indica si las maquinas del tren han sido encendidas o no.
+        /// </summary>
+        /// <value><c>true</c> si las máquinas fueron encendidas, <c>false</c> en caso contrario.</value>
+        public bool IsEngineStarted { get; private set; }
+        private static int count{ get; set;}
+
+        private string linea{ get; set;}
+
+        //Variable linea (identificadora) Creada
+        
+
+
+
+
+        /// <summary>
+        /// Enciende las máquinas del tren.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> si las máquinas pueden ser encendidas, <c>false</c> en caso contrario.
+        /// </returns>
+        
+        public Train(string linea) {
+            this.linea = linea;
+            Train.count++;
+            Console.WriteLine("Tren Numero: "+ count);
+            Console.WriteLine("Linea de tren:  "+ linea);
+        }
+        // cada vez que hago un var train = new Train(); en el main se ejecuta el codigo de arriba.
+
+        // get y set es para poder instansear valores dentro de el train y controlar el acceso a los atributos de la clase
+
+        ~Train()
+        {
+            count--;
+        }
+        public bool StartEngines()
+        {
+            if (this.IsEngineStarted)
+            {
+                Console.Write("The engines are already running");
+                return false;
+            }
+
+            this.IsEngineStarted = true;
+            Console.Write("Engines on");
+            return true;
+        }
+
+        /// <summary>
+        /// Detiene las máquinas del tren.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> si las máquinas pueden ser detenidas, <c>false</c> en caso contrario.
+        /// </returns>
+        public bool StopEngines()
+        {
+            if (this.IsEngineStarted)
+            {
+                this.IsEngineStarted = false;
+                Console.Write("Engines off");
+                return true;
+            }
+
+            Console.Write("The engines are already stopped");
+            return this.IsEngineStarted;
+        }
+    }
+}
